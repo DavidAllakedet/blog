48343e93b9c37ea9a585731727272b48a13566a7
David Allakedet <allakedetdavid@gmail.com>
2024-07-12 05:11:33 +0000

    filtre par category avec where

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,7 +26,10 @@ class PostController extends Controller
     public function postsByCategory(Category $category)
     {
         return view('posts.index', [
-            'posts' => $category->posts()->paginate(10),
+            // 'posts' => $category->posts()->latest()->paginate(10),
+            'posts' => Post::where(
+                'category_id', $category->id
+            )->latest()->paginate(10)
         ]);
 
     }
